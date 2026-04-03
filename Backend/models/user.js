c369a25578acefa2d8d6c73d93fb96242a82ff76
Sriyathurshan <sri05yathurshan@gmail.com>
2026-04-03 10:39:28 +0530

    user schema added

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,4 +1,4 @@
-const mongoose=required("mongoose")
+const mongoose=require("mongoose")
 const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema(
@@ -30,3 +30,21 @@ const userSchema = new mongoose.Schema(
     },
     {timestamps : true}
 )
+
+//password Hash middleware
+
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next() ;
+    const salt=await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+    next()
+
+})
+
+//match user entered pasword to hashed password
+
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
+
+module.exports=mongoose.model("User",userSchema)
