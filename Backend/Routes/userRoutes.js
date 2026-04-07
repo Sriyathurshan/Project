4fa532e877814988dc6447ce61734ce0545ad6bf
Sriyathurshan <sri05yathurshan@gmail.com>
2026-04-07 13:54:07 +0530

    implement user login with JWT token generation

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -14,24 +14,76 @@ router.post("/register",async(req,res)=> {
         //Registration Logic
         let user=await User.findOne({email});
 
-        if  (user) return res.status(400).json({message:"user Already exists"});
+        if  (user) return res.status(400).json({message:"User Already exists"});
 
         user=new User({name,email,password});
         await user.save();
 
-        res.status(201).json({
-            user:{
-                _id:user._id,
-                name:user.name,
-                email:user.email,
-                role:user.role
-            }
-        });
+        // Create JWT payload
+        const payload = { user:{id:user._id,role:user.role}}
+
+        //Sign and return the token along with theuser data
+
+        jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"40h"},(err,token)=>{
+            if (err) throw err;
+
+            //send the user and token in response
+            res.status(201).json({
+                user:{
+                    _id:user._id,
+                    name:user.name,
+                    email:user.email,
+                    role:user.role
+                },
+                token
+            })
+        })
+
     }
     catch(error){
         console.log(error);
         res.status(500).send("Server Error");
     }
 });
+
+//@route POST /api/users/login
+//@desc Authenticate user
+//@access public
+router.post("/login",async(req,res) => {
+    const {email,password} =req.body
+    try{
+
+        //find user by email
+        let user = await User.findOne({email})
+        if (!user) return res.status(400).json({message:"invalid credentials"});
+        const isMatch = await user.matchPassword(password)
+
+        if(!isMatch) return res.status(400).json({message:"invalid credentials"})
+        
+        // Create JWT payload
+        const payload = { user:{id:user._id,role:user.role}}
+
+        //Sign and return the token along with theuser data
+
+        jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"40h"},(err,token)=>{
+            if (err) throw err;
+
+            //send the user and token in response
+            res.json({
+                user:{
+                    _id:user._id,
+                    name:user.name,
+                    email:user.email,
+                    role:user.role
+                },
+                token
+            })
+        })
+    } catch (error){
+    
+    console.error(error)
+    res.status(500).send("Server Error")
+    }
+})
 
 module.exports=router
