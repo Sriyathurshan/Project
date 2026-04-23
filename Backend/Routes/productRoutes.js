22cdb9ba24fa0eccc1deebf09b6723c59dcbd62e
Sriyathurshan <sri05yathurshan@gmail.com>
2026-04-23 11:43:22 +0530

    implement backend logic for other product management

--- Backend/Routes/productRoutes.js
+++ Backend/Routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const Product = require("../models/Product")
 const {protect,admin}=require("../middleware/authMiddleware")
+const react = require("react")
 
 const router=express.Router()
 
@@ -232,5 +233,81 @@ router.get("/",async(req,res)=>{
         res.status(500).json("server error")
     }
 })
+
+
+// @route GET /api/products/best-seller
+// @desc Retrieve the product with highest rating
+//@access Public
+router.get("/best-seller",async(req,res) =>{
+    try{
+        const bestSeller = await Product.findOne().sort({rating:-1})
+        if (bestSeller){
+            res.json(bestSeller)
+        }else{
+            res.status(404).json({message:"Not Found"})
+        }
+    }catch(error){
+        console.error(error)
+        res.status(500).send("Server Error")
+    }
+})
+
+// @route GET api/products/new-arrivals
+// @desc Retrieve letest 8 products - creation date
+//@access Public
+router.get("/new-arrivals",async(req,res)=>{
+    try{
+        const newArrivals = await Product.find().sort({createdAt:-1}).limit(8)
+        res.json(newArrivals)
+    }catch(error){
+        console.error(error)
+        res.status(500).json("Server Error")
+    }
+})
+
+
+//@route GET/ api/products/:id
+//@desc Get a single product details
+//@access Public
+router.get("/:id",async(req,res) =>{
+  
+    try {
+        const product=await Product.findById(req.params.id)
+        if(product){
+            res.json(product)
+        }else{
+            res.status(400).json({message:"Product Not Found"})
+        }
+    }catch(error){
+        console.error(error)
+        res.status(500).json("Server Error")
+    }
+})
+
+// @route GET API/products/similar/:id
+// @desc retrieve similar products based on the current product's gender and category
+// @access public
+router.get("/similar/:id",async(req,res) =>{
+    const{id} = req.params
+    try{
+        const product = await Product.findById(id)
+
+        if(!product){
+            return res.status(404).json({message:"Product Not Found"})
+        }
+
+        const similarProducts = await Product.find({
+            _id :{$ne:id}, //exclude the current id
+            gender:product.gender,
+            category:product.category
+        }).limit(4)
+
+        res.json(similarProducts)
+    }catch(error){
+        console.error(error)
+        res.status(500).send("Server Error")
+    }
+})
+
 
 module.exports = router;
